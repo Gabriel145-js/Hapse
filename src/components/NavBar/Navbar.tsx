f545968d45fe3034742e7872fa17a654edb0885b
Gabriel145-js <gabrielzapa456@gmail.com>
2026-02-25 22:17:59 -0300

    fix

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+
 import style from "./styles.module.scss";
 import logoMini from "../../assets/svgs/logos/logoMini.svg";
 import logoMob from "../../assets/svgs/logos/logoMobile.svg";
